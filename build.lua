1aa76bb779d347cb50bd1cb132e02a55844db14a
Robbie Smith <zoqaeski@gmail.com>
2017-12-23 16:20:34 +1100

    Initial commit of new experimental branch
    This version splits the package into separate files for ease of
    implementation.

--- build.lua
+++ build.lua
@@ -2,6 +2,7 @@
 
 -- Build script for teachingtools
 
+-- Identify the bundle and module
 bundle = ""
 module = "teachingtools"
 
@@ -11,10 +12,14 @@ installfiles = {"*.cfg", "*.sty"}
 -- Release a TDS-style zip
 packtdszip = true
 
+-- Typeset only the .tex files
+typesetfiles = {"*.tex"}
+
 -- Auto-versioning
-versionfiles = {"teachingtools.dtx"}
+versionfiles = {"teachingtools-base.dtx"}
 versionform = "ProvidesExplPackage"
 
-kpse.set_program_name("kpsewhich")
-dofile(kpse.lookup("l3build.lua"))
+-- Find and run the build system
+kpse.set_program_name ("kpsewhich")
+dofile (kpse.lookup ("l3build.lua"))
 
